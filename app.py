26aed9b97cc5c5d3cc3d93de4b6e438f5626a946
AC583 <adrienchen29@gmail.com>
2025-07-30 15:39:21 -0400

    7/30 3:39

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import serial
 import time
 from classify import classify_image
 from imageCapture import capture_image
-
+from plantdata import plant_data
 app = Flask(__name__)
 selected_plant = None
 
@@ -12,98 +12,6 @@ ser = serial.Serial('COM7', 9600, timeout=1)
 time.sleep(2)  # Wait for Arduino to reset
 
 # Synthetic dataset of 15 plants with ideal ranges
-plant_data = {
-    "Tomato": {
-        "temp": (18, 27),
-        "humidity": (50, 70),
-        "moisture": (60, 80),
-        "light": (600, 900)
-    },
-    "Basil": {
-        "temp": (20, 30),
-        "humidity": (40, 60),
-        "moisture": (50, 75),
-        "light": (500, 800)
-    },
-    "Lettuce": {
-        "temp": (21, 32),
-        "humidity": (10, 30),
-        "moisture": (10, 30),
-        "light": (700, 1023)
-    },
-    "Potato": {
-        "temp": (16, 24),
-        "humidity": (60, 90),
-        "moisture": (70, 90),
-        "light": (300, 600)
-    },
-    "Olive": {
-        "temp": (18, 28),
-        "humidity": (50, 70),
-        "moisture": (50, 70),
-        "light": (400, 700)
-    },
-    "Strawberry": {
-        "temp": (15, 22),
-        "humidity": (60, 80),
-        "moisture": (60, 85),
-        "light": (400, 750)
-    },
-    "Rose": {
-        "temp": (20, 30),
-        "humidity": (40, 70),
-        "moisture": (55, 75),
-        "light": (600, 900)
-    },
-    "Sunflower": {
-        "temp": (18, 26),
-        "humidity": (50, 70),
-        "moisture": (65, 85),
-        "light": (500, 850)
-    },
-    "Orchid": {
-        "temp": (15, 25),
-        "humidity": (40, 60),
-        "moisture": (60, 80),
-        "light": (500, 900)
-    },
-    "Mint": {
-        "temp": (18, 28),
-        "humidity": (40, 60),
-        "moisture": (50, 75),
-        "light": (400, 700)
-    },
-    "Corn": {
-        "temp": (20, 30),
-        "humidity": (40, 70),
-        "moisture": (50, 75),
-        "light": (700, 1023)
-    },
-    "Aloe Vera": {
-        "temp": (18, 30),
-        "humidity": (20, 40),
-        "moisture": (10, 30),
-        "light": (600, 1023)
-    },
-    "Lavender": {
-        "temp": (15, 25),
-        "humidity": (30, 50),
-        "moisture": (40, 60),
-        "light": (500, 900)
-    },
-    "Okra": {
-        "temp": (18, 27),
-        "humidity": (30, 50),
-        "moisture": (20, 40),
-        "light": (300, 600)
-    },
-    "Lily": {
-        "temp": (18, 25),
-        "humidity": (60, 80),
-        "moisture": (60, 80),
-        "light": (200, 500)
-    }
-}
 
 
 def parse_sensor_data():
@@ -191,7 +99,7 @@ def check_ranges(sensor_data, ideal_ranges):
     return suggestions
 
 
-@app.route('/stream', methods=['GET', 'POST'])
+@app.route('/show_stream', methods=['GET', 'POST'])
 def my_scheduled_function():
     # Your continuous or periodic task logic here
     capture_image(1)
---
+++ plantdata.py
@@ -0,0 +1,92 @@
+plant_data = {
+    "Tomato": {
+        "temp": (18, 27),
+        "humidity": (50, 70),
+        "moisture": (60, 80),
+        "light": (600, 900)
+    },
+    "Basil": {
+        "temp": (20, 30),
+        "humidity": (40, 60),
+        "moisture": (50, 75),
+        "light": (500, 800)
+    },
+    "Lettuce": {
+        "temp": (21, 32),
+        "humidity": (10, 30),
+        "moisture": (10, 30),
+        "light": (700, 1023)
+    },
+    "Potato": {
+        "temp": (16, 24),
+        "humidity": (60, 90),
+        "moisture": (70, 90),
+        "light": (300, 600)
+    },
+    "Olive": {
+        "temp": (18, 28),
+        "humidity": (50, 70),
+        "moisture": (50, 70),
+        "light": (400, 700)
+    },
+    "Strawberry": {
+        "temp": (15, 22),
+        "humidity": (60, 80),
+        "moisture": (60, 85),
+        "light": (400, 750)
+    },
+    "Rose": {
+        "temp": (20, 30),
+        "humidity": (40, 70),
+        "moisture": (55, 75),
+        "light": (600, 900)
+    },
+    "Sunflower": {
+        "temp": (18, 26),
+        "humidity": (50, 70),
+        "moisture": (65, 85),
+        "light": (500, 850)
+    },
+    "Orchid": {
+        "temp": (15, 25),
+        "humidity": (40, 60),
+        "moisture": (60, 80),
+        "light": (500, 900)
+    },
+    "Mint": {
+        "temp": (18, 28),
+        "humidity": (40, 60),
+        "moisture": (50, 75),
+        "light": (400, 700)
+    },
+    "Corn": {
+        "temp": (20, 30),
+        "humidity": (40, 70),
+        "moisture": (50, 75),
+        "light": (700, 1023)
+    },
+    "Aloe Vera": {
+        "temp": (18, 30),
+        "humidity": (20, 40),
+        "moisture": (10, 30),
+        "light": (600, 1023)
+    },
+    "Lavender": {
+        "temp": (15, 25),
+        "humidity": (30, 50),
+        "moisture": (40, 60),
+        "light": (500, 900)
+    },
+    "Okra": {
+        "temp": (18, 27),
+        "humidity": (30, 50),
+        "moisture": (20, 40),
+        "light": (300, 600)
+    },
+    "Lily": {
+        "temp": (18, 25),
+        "humidity": (60, 80),
+        "moisture": (60, 80),
+        "light": (200, 500)
+    }
+}
